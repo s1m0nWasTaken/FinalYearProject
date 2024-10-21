80a5e2ba1646fc673ccb7e616de6e1dde27355a3
Simon <szymonszulc2002@gmail.com>
2024-10-21 23:35:20 +0100

    Pushing fix on directory, added boiler-plate to continue tomorrow.

--- votefunc/vote/views.py
+++ votefunc/vote/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Vote
-from .serializers import VoteSerializer
+from .models import Vote, Election
+from .serializers import VoteSerializer, ElectionSerializer
 
 
 @api_view(['POST'])
@@ -42,3 +42,28 @@ def cast_vote(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     print(serializer.errors)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def register_election(request):
+    serializer = ElectionSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def list_elections(request):
+    elections = Election.objects.all()
+    serializer = ElectionSerializer(elections, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_election(request, pk):
+    try:
+        election = Election.objects.get(pk=pk)
+    except Election.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = ElectionSerializer(election)
+    return Response(serializer.data)
+
+
